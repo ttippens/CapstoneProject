96475a4e3e4c3454c93d9927820994e684493731
ttippens <49510050+ttippens@users.noreply.github.com>
2020-06-25 11:31:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Database connection string
-DATABASE_URL = "postgres://rgtkgtbnphmdae:9e38766e18b328da6d4be1ad4d2eb155dcff236e7d46aad6b148fef38498692d@ec2-34-232-147-86.compute-1.amazonaws.com:5432/d5o0brthggfn9i"
+DATABASE_URL = "postgres://postgres:Tr@v1st1p@localhost:5432/casting_agency"
 #Supress warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
